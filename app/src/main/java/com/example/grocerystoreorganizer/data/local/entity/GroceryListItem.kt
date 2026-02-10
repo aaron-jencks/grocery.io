a43b7c46c8368d0823a8603d4f7fe02fd9aab79c
aaron-jencks <aaronjencks@aol.com>
2026-02-09 22:02:31 -0500

    added uniqueness constraint

--- app/src/main/java/com/example/grocerystoreorganizer/data/local/entity/GroceryListItem.kt
+++ app/src/main/java/com/example/grocerystoreorganizer/data/local/entity/GroceryListItem.kt
@@ -2,9 +2,10 @@ package com.example.grocerystoreorganizer.data.local.entity
 
 import androidx.room.ColumnInfo
 import androidx.room.Entity
+import androidx.room.Index
 import androidx.room.PrimaryKey
 
-@Entity(tableName = "list_items")
+@Entity(tableName = "list_items", indices = [Index(value = ["productId"], unique = true)])
 data class GroceryListItem(
     @PrimaryKey(autoGenerate = true) @ColumnInfo(name = "rowid") val id: Int,
     val productId: Int,
--- app/src/main/java/com/example/grocerystoreorganizer/data/local/entity/Store.kt
+++ app/src/main/java/com/example/grocerystoreorganizer/data/local/entity/Store.kt
@@ -2,9 +2,10 @@ package com.example.grocerystoreorganizer.data.local.entity
 
 import androidx.room.ColumnInfo
 import androidx.room.Entity
+import androidx.room.Index
 import androidx.room.PrimaryKey
 
-@Entity(tableName = "stores")
+@Entity(tableName = "stores", indices = [Index(value = ["address"], unique = true)])
 data class Store(
     @PrimaryKey(autoGenerate = true) @ColumnInfo("rowid") val id: Int,
     val name: String? = null,
